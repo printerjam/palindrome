bd61597705a09c936a60ad6b75c12895222afc5a
Etienne <etienne.ledoux@voxtelecom.co.za>
2020-02-27 12:33:43 +0200

    best attempt so far at this challenge

---
+++ p.py
@@ -0,0 +1,40 @@
+
+string = 'adskjfhaaaaenmeawprunxczujweflkjdrurshdshmadamlsakmamadamamjdsalkqwbkqw'
+#string = 'aaaaaa'
+
+palindrome = ''
+substring_begin = 0
+substring_end = 0
+
+def stop_process(substring_begin, substring_end):
+    if substring_begin < 0 or substring_end > len(string):
+        return True
+
+    return False
+
+for i in range(1, len(string)):
+
+    substring_begin = i-1    
+    substring_end = i+2
+
+    if stop_process(substring_begin, substring_end):
+        break
+
+    substring = string[substring_begin:substring_end]
+    while substring == substring[::-1]:
+        if len(substring) > len(palindrome):
+            palindrome = substring
+            position_begin = substring_begin
+            position_end = substring_end
+
+        substring_begin = substring_begin-1
+        substring_end = substring_end+1
+
+        if stop_process(substring_begin, substring_end):
+            break
+
+        substring = string[substring_begin:substring_end]
+
+print('\nString ['+str(len(string))+']: '+string)
+print('Position: '+string[:position_begin]+'\033[1m\033[4m'+palindrome+'\033[0m'+string[position_end:])
+print('Palindrome ['+str(len(palindrome))+']: '+palindrome+'\n')
