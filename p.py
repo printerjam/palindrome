5febf08c6bba3c545f829f8918db9c64a5c36d83
Etienne <etienne.ledoux@voxtelecom.co.za>
2020-03-16 14:41:07 +0200

    Second attempt. This should do it

--- p.py
+++ p.py
@@ -1,17 +1,8 @@
-"""
-After my first attempt at this, I went to look at solutions on the internet to compare and realized this solution is broken.
 
-It will work with palindromes like 'abcba' but not 'abccba'.
-
-Such a stupid mistake. I will try again and update.
-"""
-
-string = 'adskjfhaaaaenmeawprunxczujweflkjdrurshdshmadamlsakmamadamamjdsalkqwbkqw'
+string = 'adskjfhaaaaenmeawprunxczujweflkjdrurshdshmadamlsakmamaddamamjdsalkqwbkqw'
 #string = 'aaaaaa'
 
-palindrome = ''
-substring_begin = 0
-substring_end = 0
+palindrome = { 'word': '', 'position_begin': 0, 'posistion_end': 0 }
 
 def stop_process(substring_begin, substring_end):
     if substring_begin < 0 or substring_end > len(string):
@@ -19,20 +10,14 @@ def stop_process(substring_begin, substring_end):
 
     return False
 
-for i in range(1, len(string)):
-
-    substring_begin = i-1    
-    substring_end = i+2
-
-    if stop_process(substring_begin, substring_end):
-        break
-
+def palindrome_check(substring_begin,substring_end):
+    global palindrome
     substring = string[substring_begin:substring_end]
     while substring == substring[::-1]:
         if len(substring) > len(palindrome):
-            palindrome = substring
-            position_begin = substring_begin
-            position_end = substring_end
+            palindrome['word'] = substring
+            palindrome['position_begin'] = substring_begin
+            palindrome['position_end'] = substring_end
 
         substring_begin = substring_begin-1
         substring_end = substring_end+1
@@ -42,6 +27,13 @@ for i in range(1, len(string)):
 
         substring = string[substring_begin:substring_end]
 
+for i in range(1, len(string)):
+
+    if stop_process(i-1, i+2):
+        break
+    palindrome_check(i-1, i+2)
+    palindrome_check(i, i+2)
+
 print('\nString ['+str(len(string))+']: '+string)
-print('Position: '+string[:position_begin]+'\033[1m\033[4m'+palindrome+'\033[0m'+string[position_end:])
-print('Palindrome ['+str(len(palindrome))+']: '+palindrome+'\n')
+print('Position: '+string[:palindrome.get('position_begin')]+'\033[1m\033[4m'+palindrome.get('word')+'\033[0m'+string[palindrome.get('position_end'):])
+print('Palindrome ['+str(len(palindrome.get('word')))+']: '+palindrome.get('word')+'\n')
